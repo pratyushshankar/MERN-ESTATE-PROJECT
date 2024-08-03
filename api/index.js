09cb35b57773aaff7b72e3216c4c83e73fa2fa26
Pratyush shankar <pratyushshanker07@gmail.com>
2024-08-03 15:50:32 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from  'dotenv';
+dotenv.config();
+
+// mongodb+srv://pratyush:PratyushEstate@mern-estate.8vg12st.mongodb.net/"
 
 const app = express();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDb!');
+}) .catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000')

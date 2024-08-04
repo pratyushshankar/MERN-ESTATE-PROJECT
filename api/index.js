ed846511d3a966e96320143baeea9fd08d45ca45
Pratyush shankar <pratyushshanker07@gmail.com>
2024-08-04 12:42:15 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from  'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
-// mongodb+srv://pratyush:PratyushEstate@mern-estate.8vg12st.mongodb.net/"
 
 const app = express();
+app.use(express.json());
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to MongoDb!');
 }) .catch((err)=>{
@@ -17,4 +18,5 @@ app.listen(3000,()=>{
     console.log('server is running on port 3000')
 });
 
- app.use("/api/user",userRouter);
+ app.use('/api/user',userRouter);
+ app.use('/api/auth',authRouter);

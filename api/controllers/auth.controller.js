2e8c2aa297c180391ce2bbaf552caf744bc220af
Pratyush shankar <pratyushshanker07@gmail.com>
2024-08-05 20:00:44 +0530

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+
 import jwt from 'jsonwebtoken';
 
 
@@ -23,7 +25,7 @@ export const signin = async (req,res,next)=>{
     const validUser = await User.findOne({email});
     if(!validUser) return next(errorHandler(404,'User not found'));
     const validPassword = bcryptjs.compareSync(password,validUser.password);
-    if(!validPassword) return next(errorHandler(401,'Wrong Credentials !'));
+    if(!validPassword) return next(errorHandler(401,'Wrong Credential !'));
     const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
     const{password:pass,...rest} =validUser._doc;
           res.cookie('access_token',token,{httpOnly:true})

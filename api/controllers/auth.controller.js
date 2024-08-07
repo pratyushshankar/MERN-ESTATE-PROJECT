d21ca7c04d476e945c52b3bfd77d740bbf7f8d92
Pratyush shankar <pratyushshanker07@gmail.com>
2024-08-07 20:18:12 +0530

    add signout functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -73,3 +73,14 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+// signout Handler
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -47,6 +47,18 @@ const userSlice = createSlice({
         state.error =action.payload;
         state.loading=false;
       },
+     signOutUserStart:(state)=>{
+        state.loading=true
+      },
+     signOutUserSuccess:(state)=>{
+        state.currentUser=null;
+        state.loading=false,
+        state.error=null;
+      },
+     signOutUserFailure:(state,action) =>{
+        state.error =action.payload;
+        state.loading=false;
+      },
 
     }
 })
@@ -61,7 +73,10 @@ export const{
     updateUserSuccess,
     deleteUserStart,
     deleteUserSuccess,
-    deleteUserFailure
+    deleteUserFailure,
+    signOutUserStart,
+    signOutUserSuccess,
+    signOutUserFailure
 } = userSlice.actions
 
 export default userSlice.reducer
